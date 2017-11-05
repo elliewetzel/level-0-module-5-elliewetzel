5f07a3eeb2858bbb2fbfb80cf680f1227ee97cff
Buskcoin <keith.groves@jointheleague.org>
2017-11-04 18:49:32 -0700

    A rendition of Matt's coding challenge
    Very difficult

---
+++ src/EvilPolygonsChallenge.java
@@ -0,0 +1,29 @@
+package snippet;
+
+import java.awt.Color;
+
+import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class EvilPolygonsChallenge {
+	
+	public static void main(String[] args) {
+		//1. Create a robot
+
+		//2. Set the speed to 100
+
+		int colorChoice=JOptionPane.showOptionDialog(null, "hello", "title", JOptionPane.INFORMATION_MESSAGE, JOptionPane.DEFAULT_OPTION, null, new String[]{"Red","Yellow"}, 0);
+		
+		//3. Set the pen color of the robot according to the value of the colorChoise variable
+		
+		//4. Ask the use how many polygons they want to be drawn.
+		
+		//5. Use the robot to draw the number of polygons the user requested. Hint: this may require a nested for loop
+		
+		//6. Make it so your shapes do not overlap
+
+		//7. Challenge: add more colors to the Option Dialog.
+	}
+}
+
